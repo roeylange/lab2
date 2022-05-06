f3d2f94f94d3a0c188270da78b8080cb3eb11e52
roeylange <roey.lange@gmail.COM>
2022-05-06 16:01:04 +0300

    First commit

---
+++ task2C/menu_map.c
@@ -0,0 +1,104 @@
+﻿#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char encrypt(char c){
+    if(c>=0x41 && c<=0x7a)
+        return c+2;
+    else
+        return c;
+}
+
+char decrypt(char c){
+    if(c>=0x41 && c<=0x7a)
+        return c-2;
+    else
+        return c;
+}
+
+char dprt(char c){
+    printf("%d\n",c);
+    return c;
+}
+
+char cprt(char c){
+    if(c>=0x41 && c<=0x7a)
+        printf("%c\n",c);
+    else
+        printf("*\n");
+    return c;
+}
+
+char my_get(char c){
+    return fgetc(stdin);
+}
+
+char quit(char c){
+    if(c=='q')
+        exit(0);
+    else
+        return c;
+}
+
+
+char censor(char c) {
+    if(c == '!')
+        return '*';
+    else
+        return c;
+}
+
+char* map(char *array, int array_length, char (*f) (char)){
+    char* mapped_array = (char*)(malloc(array_length*sizeof(char)));
+    /* TODO: Complete during task 2.a */
+    for(int i=0;i<array_length;i++){
+        *(mapped_array+i) = f(*(array+i));
+    }
+    return mapped_array;
+}
+
+int* filter(int *array, int array_length, bool (*f) (int)){
+    int arraysize=0;
+    for(int i=0;i<array_length;i++){
+	if(f(array[i]))
+        	arraysize++;
+    }
+   int* mapped_array = (int*)(malloc(arraysize*4));
+   int count=0;
+   for(int i=0;i<array_length;i++){
+	if(f(array[i])){
+	  mapped_array[count]=array[i];
+	  count++;
+  }	
+
+}
+    return mapped_array;
+}
+
+
+
+int main(int argc, char **argv){
+    //char arr1[] = {'H','e','y','!'};
+    //char* arr2 = map(arr1, 4, censor);
+    //printf("%s\n", arr2);
+    //free(arr2);
+
+    //printf("%c",encrypt('C'));
+    //printf("%c",encrypt('c'));
+    int base_len = 5;
+    char arr1[base_len];
+    char* arr2 = map(arr1, base_len, my_get);
+    char* arr3 = map(arr2, base_len, encrypt);
+    char* arr4 = map(arr3, base_len, dprt);
+    char* arr5 = map(arr4, base_len, decrypt);
+    char* arr6 = map(arr5, base_len, cprt);
+    free(arr2);
+    free(arr3);
+    free(arr4);
+    free(arr5);
+    free(arr6);
+
+
+
+    return 0;
+}
---
+++ task3/menu_map.c
@@ -0,0 +1,101 @@
+﻿#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char encrypt(char c){
+    if(c>=0x41 && c<=0x7a)
+        return c+2;
+    else
+        return c;
+}
+
+char decrypt(char c){
+    if(c>=0x41 && c<=0x7a)
+        return c-2;
+    else
+        return c;
+}
+
+char dprt(char c){
+    printf("%d\n",c);
+    return c;
+}
+
+char cprt(char c){
+    if(c>=0x41 && c<=0x7a)
+        printf("%c\n",c);
+    else
+        printf("*\n");
+    return c;
+}
+
+char my_get(char c){
+    return fgetc(stdin);
+}
+
+char quit(char c){
+    if(c=='q')
+        exit(0);
+    else
+        return c;
+}
+
+
+char censor(char c) {
+    if(c == '!')
+        return '*';
+    else
+        return c;
+}
+
+char* map(char *array, int array_length, char (*f) (char)){
+    char* mapped_array = (char*)(malloc(array_length*sizeof(char)));
+    /* TODO: Complete during task 2.a */
+    for(int i=0;i<array_length;i++){
+        mapped_array[i] = f(array[i]);
+    }
+    return mapped_array;
+}
+
+struct fun_desc {
+    char *name;
+    char (*fun)(char);
+};
+
+int main(int argc, char **argv){
+    char charArray[5]="";
+    char* carray = charArray;
+    struct fun_desc menu[] = { {"Censor",censor},
+                               {"Encrypt", encrypt},
+                               {"Decrypt", decrypt},
+                               {"Print dec", dprt},
+                               {"Print string", cprt},
+                               {"Get string", my_get},
+                               {"Quit", quit},
+                               {NULL, NULL}};
+    int bound = 0;
+    while(menu[bound+1].name!=NULL)
+        bound++;
+    int command;
+    char c;
+    while(1) {
+        printf("Please choose a function:\n");
+        for (int i = 0; i <= bound; i++) {
+            printf("%d) %s\n", i, menu[i].name);
+        }
+        printf("Option: ");
+        scanf("%d",&command);
+        c = fgetc(stdin);
+        if (command != 10) {
+            if (command >= 0 && command <= bound) {
+                printf("Within bounds\n");
+                carray = map(carray, 5,menu[command].fun);
+                printf("DONE.\n");
+            }
+            else {
+                printf("Not within bounds\n");
+                exit(1);
+            }
+        }
+    }
+}
